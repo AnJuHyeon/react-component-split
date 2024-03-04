9305f7064c0e29387cfde0c1120719b8d24fa607
AnJuHyun <anju3802@naver.com>
2024-03-04 22:42:23 +0900

    react 를 이용해 성능개선에 필요한 것들 알아봄

---
+++ src/components/A.js
@@ -0,0 +1,22 @@
+//rafce 치면 아래처럼 나옴
+import React from 'react'
+
+const A = ({message,posts}) => {
+  return (
+    <div>
+      <h1>A Component</h1>
+      <p>{message}</p>
+      <ul>
+        {posts.map(post=>{
+            return (
+                <li key={post.id}>
+                    <p>{post.title}</p>
+                </li>
+            )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default A
